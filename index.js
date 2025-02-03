e52f24d2c7f384a14a3dea373dbfec32c7a0e429
nicolo_canevari <nico.canevari@outlook.it>
2025-02-03 17:51:29 +0100

    [FEAT] NJS: adesso con watch quando salvo il file aggiorna automaticamente il terminale senza fermare il server

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// Stampo il risultato sul terminale => node index.js 
+// Stampo il risultato sul terminale => node index.js / npm run start
 // console.log("Hello World");
 
-// Modifico il codice per utilizzare chalk e colorare il testo, uso require per importare il pacchetto nel mio codice 
-// const chalk = require('chalk');
+// ! Modifico il codice per utilizzare chalk e colorare il testo, uso require per importare il pacchetto nel mio codice 
+// ! const chalk = require('chalk');
 
 // Uso import al posto di require perchè chalck è in versione "^5.4.1", l'API è cambiata e il modo di usare i colori è diverso rispetto alle versioni precedenti
 import chalk from 'chalk';
 
-// Stampo il risultato sul terminale => node index.js / npm run start
-console.log(chalk.red.bold('Hello World'));
+// Stampo il risultato sul terminale => npm run watch (grazie a watch)
+console.log(chalk.red.bold('Hello Boolean'));
 
 
 
